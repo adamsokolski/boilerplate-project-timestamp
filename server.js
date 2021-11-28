8c20b4ed8313916874637309c43dd7cc7803ba36
idKrazu <idkrazu@gmail.com>
2021-11-28 18:30:07 +0100

    ✨ /api/ working

--- server.js
+++ server.js
@@ -2,31 +2,51 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+require("dotenv").config();
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+// your first API endpoint...
+app.get("/api/:date", function (req, res) {
+  const regex = /^-\d+$|^\d+$/g;
+  const onlyDigits = regex.test(req.params.date);
+  // console.log(`${req.params.date} ` + notOnlyDigits.test(req.params.date));
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  let milliseconds = Date.now();
+  if (onlyDigits) {
+    milliseconds = parseInt(req.params.date);
+  } else {
+    const userDate = new Date(req.params.date);
+    milliseconds = userDate.getTime();
+  }
+
+  const dateObject = new Date(milliseconds);
+  const dateString = dateObject.toUTCString();
+  const jsonData =
+    dateString !== "Invalid Date"
+      ? {
+          unix: milliseconds,
+          utc: dateObject.toUTCString(),
+        }
+      : { error: "Invalid Date" };
+
+  res.json(jsonData);
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
